74ba1dfd9629dc0c512613184f6404755051f162
Andrei Tambovtsev <ait27ru@gmail.com>
2024-03-29 21:46:21 +1300

    Refactor so that video-streaming service sends a message to history via RabbitMQ in one-to-one manner.

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const http = require("http");
+const amqp = require("amqplib");
 
 if (!process.env.PORT) {
   throw new Error(
@@ -8,48 +8,38 @@ if (!process.env.PORT) {
   );
 }
 
-const PORT = process.env.PORT;
-
-//
-// Send the "viewed" to the history microservice.
-//
-function sendViewedMessage(videoPath) {
-  const postOptions = {
-    // Options to the HTTP POST request.
-    method: "POST", // Sets the request method as POST.
-    headers: {
-      "Content-Type": "application/json", // Sets the content type for the request's body.
-    },
-  };
-
-  const requestBody = {
-    // Body of the HTTP POST request.
-    videoPath: videoPath,
-  };
-
-  const req = http.request(
-    // Send the "viewed" message to the history microservice.
-    "http://history/viewed",
-    postOptions
+if (!process.env.RABBIT) {
+  throw new Error(
+    "Please specify the name of the RabbitMQ host using environment variable RABBIT"
   );
-
-  req.on("close", () => {
-    console.log("Sent 'viewed' message to history microservice.");
-  });
-
-  req.on("error", (err) => {
-    console.error("Failed to send 'viewed' message!");
-    console.error((err && err.stack) || err);
-  });
-
-  req.write(JSON.stringify(requestBody)); // Write the body to the request.
-  req.end(); // End the request.
 }
+
+const PORT = process.env.PORT;
+const RABBIT = process.env.RABBIT;
 
 //
 // Application entry point.
 //
 async function main() {
+  console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
+
+  const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
+
+  console.log("Connected to RabbitMQ.");
+
+  const messageChannel = await messagingConnection.createChannel(); // Creates a RabbitMQ messaging channel.
+
+  //
+  // Send the "viewed" to the history microservice.
+  //
+  function sendViewedMessage(messageChannel, videoPath) {
+    console.log(`Publishing message on "viewed" queue.`);
+
+    const msg = { videoPath: videoPath };
+    const jsonMsg = JSON.stringify(msg);
+    messageChannel.publish("", "viewed", Buffer.from(jsonMsg)); // Publishes message to the "viewed" queue.
+  }
+
   const app = express();
 
   app.get("/video", async (req, res) => {
@@ -59,13 +49,13 @@ async function main() {
     const stats = await fs.promises.stat(videoPath);
 
     res.writeHead(200, {
-      "content-length": stats.size,
-      "content-type": "video/mp4",
+      "Content-Length": stats.size,
+      "Content-Type": "video/mp4",
     });
 
     fs.createReadStream(videoPath).pipe(res);
 
-    sendViewedMessage(videoPath); // Sends the "viewed" message to indicate this video has been watched.
+    sendViewedMessage(messageChannel, videoPath); // Sends the "viewed" message to indicate this video has been watched.
   });
 
   app.listen(PORT, () => {
